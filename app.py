eff9a21f824e55020d7c1fb018f0b04c4dc37454
nathanPM104 <79662158+nathanPM104@users.noreply.github.com>
2021-06-01 22:11:41 -0700

    HWADD

--- app.py
+++ app.py
@@ -38,11 +38,54 @@ app = Flask(__name__)
 
 @app.route("/")
 def welcome():
-    ""list all available api routes.""
     return(
         f"Available Routes:<br/>"
         f"/api/v1.0/precipitation<br/>"
         f"/api/v1.0/stations<br/>"
         f"/api/v1.0/tobs<br/>"
-        f"/api/v1.0/temp/start/end
+        f"/api/v1.0/temp/start/end"
     )
+
+
+@app.route("/api/v1.0/precipitation")
+
+def precipitation():
+    results = session.query(Measurement.date, Measurement.prcp).filter(Measurement.date >= "2016-08-24").\
+    filter(Measurement.date <= "2017-08-23").all()
+
+    precip = {date: prcp for date, prcp in results}
+    return jsonify(precip)
+    
+
+@app.route("/api/v1.0/stations")
+
+def stations():
+    results = session.query(Station.station, Station.name).order_by(Station.name.desc()).all()
+    
+    stations = list(np.ravel(results))
+    return jsonify(stations)
+
+
+
+@app.route("/api/v1.0/tobs")
+
+def active_station():
+    station = 'USC00511918'
+
+    results = session.query(Measurement.station, Measurement.date, Measurement.tobs).\
+    filter(Measurement.station == station).\
+    filter(Measurement.date >= "2014-10-30").\
+    filter(Measurement.date <= "2015-10-30").all()
+    
+    station_temps = list(np.ravel(results))
+
+    # Return the results
+    return jsonify(temps)
+
+@app.route("/api/v1.0/temp/start/end")
+
+def temp
+
+    
+if __name__ == '__main__':
+    app.run()
